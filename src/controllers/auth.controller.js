751bd612fbb6b5be534389124acaaa8d6f78074e
Abhyudaya2005 <tiwarishikhar608@gmail.com>
2026-09-18 00:09:21 +0530

    logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
 
 
 /**
@@ -32,11 +33,13 @@ async function registerUserController(req,res){
 
     const hash= await bcrypt.hash(password,10)
 
-    const User = await userModel({
-        username,
-        email,
-        password:hash
-    })
+    const User = new userModel({
+    username,
+    email,
+    password: hash
+})
+
+await User.save()
     const token = jwt.sign(
         {id:User._id,username: User.username},
         process.env.JWT_SECRET,
@@ -91,7 +94,47 @@ async function loginUserController(req,res){
         })
 }
 
+/**
+ * @name logoutUserController
+ * @description clear token from the cookie and add the token to the blacklist
+ * @access Public
+ */
+
+async function logoutUserController(req,res){
+    const token = req.cookies.token
+
+    if(token){
+        await tokenBlacklistModel.create({token})
+    }
+
+    res.clearCookie("token")
+    res.status(200).json({
+        message:"User logged out successfully"
+    })
+}
+
+/**
+ * @name getMeController
+ * @description Get the details of the logged-in user
+ * @access Private  
+ */
+
+async function getMeController(req,res){
+
+    const user = await userModel.findById(req.user.id)
+    res.status(200).json({
+        message:"User details fetched successfully",
+        user:{
+            id:user._id,
+            username:user.username, 
+            email:user.email
+        }
+    })
+}
+
 module.exports = {
     registerUserController,
-    loginUserController
-}   
+    loginUserController,
+    logoutUserController,
+    getMeController
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const {Router}  = require("express")
 const authController = require("../controllers/auth.controller")
-
+const authMiddleware = require("../middlewares/auth.middleware")
 const authRouter = Router();
 
 /**
@@ -9,12 +9,30 @@ const authRouter = Router();
  * @access Public
  */
 authRouter.post("/register",authController.registerUserController)
+
 /**
  * @route POST /api/auth/login
  * @description Login a user with email and password
  * @access Public
  */
 authRouter.post("/login",authController.loginUserController)
+
+/** 
+ * @route GET /api/auth/logout
+ * @description clear token from the cookie and add the token to the blacklist
+ * @access Public
+ */
+authRouter.get("/logout",authController.logoutUserController)
+
+/**
+ * @route GET /api/auth/get-me
+ * @description Get the details of the logged-in user
+ * @access Private
+ */
+
+authRouter.get("/get-me",authMiddleware.authuser,)
+ 
+
  
 
 
